880cc48a5b0a2c4dc204755be2db22a5bb965bdb
pro804 <petsoulis.appdev@gmail.com>
2025-09-09 11:02:13 +0300

    feat: implement data fetching with optimized useEffect - Add useEffect and useState hooks for API data and loading state management - Create fetchJobs async function to retrieve data from external API - Implement loading state with styled spinner animation during data fetch -Use empty dependency array in useEffect to ensure data fetches only once on component mount, preventing unnecessary re-renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react";
+
+const url = "https://www.course-api.com/react-tabs-project";
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setjobs] = useState([]);
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch(url);
+      const newJobs = await response.json();
+      setjobs(newJobs);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  console.log(jobs);
   return <h2>Tabs</h2>;
 };
 export default App;
